6b56278b0af9f798cac98f3a79f9107ab5320c1f
ShailyRoy <shailyroy37@gmail.com>
2019-01-29 18:28:41 +0600

    Learn Arrow function

--- javascript/myJs.js
+++ javascript/myJs.js
@@ -1,3 +1,4 @@
+/*
 function factorial(n) {
     if (n == 0) {
     return 1;
@@ -53,3 +54,17 @@ console.log(x + y + z);
 }
 // y is not visible here
 console.log(x + z);
+*/
+//arrow function
+const horn = () => {
+    console.log("Toot");
+    };
+const power = (base, exponent) => {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+    result *= base;
+    }
+    return result;
+    };
+console.log(power(2,4));
+horn();
